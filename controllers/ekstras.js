b6bf0284e90a409a582b1874a74bc4dca3013bae
afid123456 <afid123456@gmail.com>
2023-11-16 09:17:13 +0700

    update validasi pada Models

--- controllers/ekstras.js
+++ controllers/ekstras.js
@@ -41,50 +41,77 @@ module.exports = {
           }
 
         },
-        store: async  (req, res) => {
+        store: async (req, res) => {
           try {
             const ekstra = await Ekstrakurikuler.create(req.body)
             res.status(200).json({
-              status:true,
-              data:ekstra,
+              status: true,
+              data: ekstra,
               method: req.method,
               url: req.url,
               message: "Data berhasil ditambahkan"
             })
           } catch (error) {
-            res.status(400).json({success: false})
-          }
-
-          },
-          update: async  (req, res) => {
-            try {
-                const ekstra = await Ekstrakurikuler.findByIdAndUpdate(req.params.id, req.body, {
-                  new: true,
-                  runValidators: true
-                })
-                res.json({
-                  status:true,
-                  data:ekstra,
-                  method: req.method,
-                  url: req.url,
-                  message: "Data berhasil diubah"
-                })
-            } catch (error){
-              res.status(400).json({success: false})
-            }
-
-          },
-          delete: async (req, res) => {
-            try {
-                await Ekstrakurikuler.findByIdAndDelete(req.params.id)
-                res.json({
-                  status:true,
-                  method: req.method,
-                  url: req.url,
-                  message: "Data berhasil dihapus"
-                })
-            } catch (error) {
-              res.status(400).json({success: false})
+            if (error.name === 'ValidationError') {
+              // Extract specific validation errors from 'error.errors' and send appropriate responses
+              const validationErrors = Object.keys(error.errors).map(key => {
+                return { field: key, message: error.errors[key].message };
+              });
+              res.status(400).json({
+                success: false,
+                errors: validationErrors
+              });
+            } else {
+              res.status(500).json({ success: false, message: error.message });
             }
           }
+        },        
+        update: async (req, res) => {
+          try {
+            const ekstra = await Ekstrakurikuler.findByIdAndUpdate(req.params.id, req.body, {
+              new: true,
+              runValidators: true
+            })
+            if (!ekstra) {
+              return res.status(404).json({ success: false, message: "Data not found" });
+            }
+            res.json({
+              status: true,
+              data: ekstra,
+              method: req.method,
+              url: req.url,
+              message: "Data berhasil diubah"
+            })
+          } catch (error) {
+            if (error.name === 'ValidationError') {
+              const validationErrors = Object.keys(error.errors).map(key => {
+                return { field: key, message: error.errors[key].message };
+              });
+              return res.status(400).json({
+                success: false,
+                errors: validationErrors
+              });
+            } else {
+              res.status(500).json({ success: false, message: error.message });
+            }
+          }
+        },
+        
+        delete: async (req, res) => {
+          try {
+            const deletedEkstra = await Ekstrakurikuler.findByIdAndDelete(req.params.id)
+            if (!deletedEkstra) {
+              return res.status(404).json({ success: false, message: "Data not found" });
+            }
+            res.json({
+              status: true,
+              method: req.method,
+              url: req.url,
+              message: "Data berhasil dihapus"
+            })
+          } catch (error) {
+            res.status(400).json({ success: false, message: error.message });
+          }
+        }
+        
 }
